8cebc38ba02cb2190894fdfadb8e28e6c9731da8
Sharath <sharathkrml@gmail.com>
2022-07-17 12:09:58 +0530

    added list and buy item

---
+++ contracts/NftMarketplace.sol
@@ -0,0 +1,111 @@
+// SPDX-License-Identifier: MIT
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+pragma solidity ^0.8.7;
+error NftMarketPlace__PriceMustBeAboveZero();
+error NftMarketPlace__NotApprovedForMarketplace();
+error NftMarketPlace__AlreadyListed(address nftAddress, uint256 tokenId);
+error NftMarketPlace__NotListed(address nftAddress, uint256 tokenId);
+error NftMarketPlace__PriceNotMet(address nftAddress, uint256 tokenId, uint256 price);
+error NftMarketPlace__NotOwner();
+
+contract NftMarketPlace is ReentrancyGuard {
+    struct Listing {
+        uint256 price;
+        address seller;
+    }
+    event ItemListed(
+        address indexed seller,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+    event ItemBought(
+        address indexed buyer,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+
+    // NFT address -> NFT tokenId->Listing
+    mapping(address => mapping(uint256 => Listing)) private s_listings;
+    // Seller address -> amount
+    mapping(address => uint256) private s_proceeds;
+    modifier notListed(address nftAddress, uint256 tokenId) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+        if (listing.price > 0) {
+            revert NftMarketPlace__AlreadyListed(nftAddress, tokenId);
+        }
+        _;
+    }
+    modifier isListed(address nftAddress, uint256 tokenId) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+        if (listing.price == 0) {
+            revert NftMarketPlace__NotListed(nftAddress, tokenId);
+        }
+        _;
+    }
+    modifier isOwner(
+        address nftAddress,
+        uint256 tokenId,
+        address spender
+    ) {
+        IERC721 nft = IERC721(nftAddress);
+        address owner = nft.ownerOf(tokenId);
+        if (spender != owner) {
+            revert NftMarketPlace__NotOwner();
+        }
+        _;
+    }
+
+    /*
+     * @notice Method for listing NFT to marketplace
+     * @param nftAddress: Address of NFT
+     * @param tokenId: The tokenId of NFT
+     * @param price: sale price of listed NFT
+     * @dev technically,Approve this contract to list NFT
+     * people still hold the NFT
+     */
+    function listItem(
+        address nftAddress,
+        uint256 tokenId,
+        uint256 price
+    ) external notListed(nftAddress, tokenId) isOwner(nftAddress, tokenId, msg.sender) {
+        if (price == 0) {
+            revert NftMarketPlace__PriceMustBeAboveZero();
+        }
+        IERC721 nft = IERC721(nftAddress);
+        if (nft.getApproved(tokenId) != address(this)) {
+            revert NftMarketPlace__NotApprovedForMarketplace();
+        }
+        s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
+        emit ItemListed(msg.sender, nftAddress, tokenId, price);
+    }
+
+    function buyItem(address nftAddress, uint256 tokenId)
+        external
+        payable
+        nonReentrant
+        isListed(nftAddress, tokenId)
+    {
+        Listing memory listedItem = s_listings[nftAddress][tokenId];
+        if (msg.value < listedItem.price) {
+            revert NftMarketPlace__PriceNotMet(nftAddress, tokenId, listedItem.price);
+        }
+        // We don't send moey to seller??
+        // solidity - pull over push
+        // sending the money ❌
+        // have the withdraw their money ✅
+        s_proceeds[listedItem.seller] = s_proceeds[listedItem.seller] + msg.value;
+        delete (s_listings[nftAddress][tokenId]);
+        IERC721(nftAddress).safeTransferFrom(listedItem.seller, msg.sender, tokenId);
+        emit ItemBought(msg.sender, nftAddress, tokenId, listedItem.price);
+    }
+}
+//    1. `listItem` :List NFT for sale
+//    2. `buyItem`:Buy the NFT
+//    3. `cancelItem`:Cancel Listing
+//    4. `updateListing`:Update Price
+//    5. `withdrawProceeds`:Withdraw payments for my bought NFTs
